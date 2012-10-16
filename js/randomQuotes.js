6cad487cfe1bdd969854fc6975c7e2c53fd2524c
Nikil Viswanathan <nikil@stanford.edu>
2012-10-15 19:12:00 -0700

    google analytics - tweet padding

--- js/randomQuotes.js
+++ js/randomQuotes.js
@@ -4,7 +4,7 @@ var ZOOLANDER_QUOTES =
 	"They're break-dance fighting!", 
 	"Moisture is the essence of wetness, and wetness is the essence of beauty.",
 	"Have you ever wondered if there was more to life, other than being really, really, ridiculously good looking? ",
-	"Are you an ambi-turner.",
+	"Are you an ambi-turner?",
 	"But why male models? ... Are you serious? I just told you that a moment ago. ",
 	"Cool story Hansel.",
 	"What is this? A center for ants? How can we be expected to teach children to learn how to read... if they can't even fit inside the building?",
